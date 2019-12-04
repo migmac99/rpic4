4da73c0cc8511fabe267d760f3060698106e0d2a
migmac99 <migmacthegamer@gmail.com>
2019-12-04 17:20:11 +0000

    Added Current IPV4 on startup console (sudo pm2 monit)

--- c4.js
+++ c4.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const PORT = 8080;
+const PORT = 0080;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -11,3 +11,34 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`);
 });
+
+
+
+
+'use strict';
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+	if ('IPv4' !== iface.family || iface.internal !== false) {
+	    // skips over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+	    return;
+	}
+
+	if (alias >= 1) {
+	    // this single interface has multiple ipv4 addresses
+	    console.log(ifname + ':' + alias, iface.address);
+	} else {
+	    // this interface has only one ipv4 address
+	    console.log(ifname, iface.address);
+	}
+	++alias;
+    });
+});
+
+// en0 192.168.1.101
+// eth0 10.0.0.101
