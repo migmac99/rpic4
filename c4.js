8a3743d47c788ccfb6c7b1aef74aead14d944195
migmac99 <migmacthegamer@gmail.com>
2019-12-04 23:15:38 +0000

    Cleanup

--- c4.js
+++ c4.js
@@ -10,13 +10,15 @@ if (logging) {
 }
 
 //VARIABLES
-var public_ipv4;
+var private_ipv4; //Server can be accessed through this ip if in the same network
+const PORT = 80; //C4 server port [80 is default for web]
 
 var os = require('os');
 var express = require('express');
 var fs = require('fs');
 
 var ifaces = os.networkInterfaces();
+var app = express();
 
 //LOCAL IPV4 DETECTION
 'use strict';
@@ -35,20 +37,14 @@ Object.keys(ifaces).forEach(function(ifname) {
         } else {
             // this interface has only one ipv4 address
             //console.log(ifname, iface.address);
-            public_ipv4 = iface.address;
+            private_ipv4 = iface.address;
         }
         ++alias;
     });
 });
 
 //START HTML SERVER
-const http = require('http');
-const PORT = 80;
-
-var app = express();
-
-app.use(express.static(__dirname + "/html"));
-//app.use(express.static(path.join(__dirname, "/html")));
+app.use(express.static(__dirname + "/html")); //Prevents MIME TYPE error by making html directory static and therefore usable
 
 app.get('/', function(req, res) {
     res.sendFile('html/index.html', { root: __dirname })
@@ -60,21 +56,10 @@ app.listen(PORT, () => {
     console.log(`C4 Server running on port ${PORT}.`);
 });
 
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello dev.to!\n');
-// });
-
-// server.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}.`);
-// });
-
 //Extra Logging
-
 if (logging) {
     console.log('   |');
-    console.log('   +--=[' + '\x1b[47m\x1b[30m' + ' Public IP ' + '\x1b[0m' + ']=--> ' + '\x1b[32m' + public_ipv4 + '\x1b[0m');
+    console.log('   +--=[' + '\x1b[47m\x1b[30m' + ' Private IP ' + '\x1b[0m' + ']=--> ' + '\x1b[32m' + private_ipv4 + '\x1b[0m');
     console.log('   |');
     console.log('   .'); //End Spacer
 }
