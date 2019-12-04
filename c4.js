afaadb8a5cda9f802d5a43f2ab0f0f96c2a64200
migmac99 <migmacthegamer@gmail.com>
2019-12-04 22:09:21 +0000

    changed order of c4.js

--- c4.js
+++ c4.js
@@ -1,6 +1,6 @@
 var logging = true;
 
-if (logging){
+if (logging) {
     console.log('.'); //Start Spacer
     console.log('+==========================+');
     console.log('|  Starting C4 by ' + '\x1b[36m%s\x1b[0m', 'AllTWay' + '\x1b[0m', ' |');
@@ -9,11 +9,38 @@ if (logging){
     console.log('Starting C4 by ' + '\x1b[36m%s\1', 'AllTWay' + '\x1b[0m');
 }
 
+//VARIABLES
+var public_ipv4;
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+//LOCAL IPV4 DETECTION
+'use strict';
+Object.keys(ifaces).forEach(function(ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function(iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skips over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            //console.log(ifname + ':' + alias, iface.address);
+        } else {
+            // this interface has only one ipv4 address
+            //console.log(ifname, iface.address);
+            public_ipv4 = iface.address;
+        }
+        ++alias;
+    });
+});
+
 //START HTML SERVER
 
 const http = require('http');
-
-const PORT = 0080;
+const PORT = 80;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -25,40 +52,9 @@ server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}.`);
 });
 
-
-//LOCAL IPV4 DETECTION
-
-var public_ipv4;
-
-'use strict';
-
-var os = require('os');
-var ifaces = os.networkInterfaces();
-
-Object.keys(ifaces).forEach(function (ifname) {
-    var alias = 0;
-
-    ifaces[ifname].forEach(function (iface) {
-	if ('IPv4' !== iface.family || iface.internal !== false) {
-	    // skips over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-	    return;
-	}
-
-	if (alias >= 1) {
-	    // this single interface has multiple ipv4 addresses
-	    //console.log(ifname + ':' + alias, iface.address);
-	} else {
-	    // this interface has only one ipv4 address
-	    //console.log(ifname, iface.address);
-	    public_ipv4 = iface.address;
-	}
-	++alias;
-    });
-});
-
 //Extra Logging
 
-if (logging){
+if (logging) {
     console.log('   |');
     console.log('   +--=[' + '\x1b[47m\x1b[30m' + ' Public IP ' + '\x1b[0m' + ']=--> ' + '\x1b[32m' + public_ipv4 + '\x1b[0m');
     console.log('   |');
