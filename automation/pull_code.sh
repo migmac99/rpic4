71ac03217e5765deb7aeb98b3929edfe30f08309
migmac99 <migmacthegamer@gmail.com>
2022-04-13 13:09:01 +0100

    Update pull_code.sh

--- automation/pull_code.sh
+++ automation/pull_code.sh
@@ -1,15 +1,2 @@
-
-git checkout -b master
-git remote update
-
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-    sh automation/pull_code.sh     
-fi
+git reset --hard origin/master
+git pull master
