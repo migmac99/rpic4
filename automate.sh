081e517b94dcc7409ac5206dfbaa2feb5a4faaf6
migmac99 <migmacthegamer@gmail.com>
2022-04-13 12:15:42 +0100

    Create automate.sh

---
+++ automate.sh
@@ -0,0 +1,15 @@
+
+git checkout -b master
+git remote update
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    sh pull_code.sh     
+fi
