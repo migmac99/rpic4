c775f5b49e7cc9b364619c6866a49d5e7df07776
migmac99 <migmacthegamer@gmail.com>
2022-05-06 21:34:34 +0100

    keypad test 1

---
+++ KeypadDefault.js
@@ -0,0 +1,21 @@
+var Keypad = require("rpi-keypad");
+
+var input = new Keypad(
+    [
+        ["1", "2", "3", "A"],
+        ["4", "5", "6", "B"],
+        ["7", "8", "9", "C"],
+        ["*", "0", "#", "D"],
+    ], // keypad layout
+    [40, 38, 36, 32], // row GPIO pins
+    [37, 35, 33, 31] // colum GPIO pins
+);
+
+setInterval(() => {
+    var key = keypad.getKey();
+    if (key != null) {
+        console.log("key pressed: " + key);
+    } else {
+        console.log("no key pressed");
+    }
+}, 100);
---
+++ keypad.js
@@ -0,0 +1,21 @@
+var Keypad = require("rpi-keypad");
+
+var input = new Keypad(
+    [
+        ["1", "2", "3", "A"],
+        ["4", "5", "6", "B"],
+        ["7", "8", "9", "C"],
+        ["*", "0", "#", "D"],
+    ], // keypad layout
+    [25, 8, 7, 1], // row GPIO pins
+    [12, 16, 20, 21] // colum GPIO pins
+);
+
+setInterval(() => {
+    var key = keypad.getKey();
+    if (key != null) {
+        console.log("key pressed: " + key);
+    } else {
+        console.log("no key pressed");
+    }
+}, 100);
