f77326afe90bfe4e422e738d1cdd7abf0c170eb1
migmac99 <migmacthegamer@gmail.com>
2022-05-07 12:11:17 +0100

    keypad test 2

--- index.js
+++ index.js
@@ -66,6 +66,32 @@ function start() {
     LogCustom('  DEBUG ', colors.cyan, 'Config:', config)
 }
 
+
+////////////////////////////////////////////////////////////////////////
+
+var Keypad = require("rpi-keypad")
+
+var input = new Keypad(
+    [
+        ["1", "2", "3", "A"],
+        ["4", "5", "6", "B"],
+        ["7", "8", "9", "C"],
+        ["*", "0", "#", "D"],
+    ], // keypad layout
+    [40, 38, 36, 32], // row GPIO pins
+    [37, 35, 33, 31], // colum GPIO pins
+    // additional:
+    true, // use key press events
+    100 // interval in ms to poll for key events
+)
+
+input.on("keypress", (key) => {
+    config[0].msg = "Key pressed: " + key
+})
+
+////////////////////////////////////////////////////////////////////////
+
+
 function update(line, tick) {
     cfg = config[line];
 
@@ -86,19 +112,6 @@ function update(line, tick) {
     //     }
     // }, 100)
 }
-
-var Keypad = require("rpi-keypad")
-var input = new Keypad(
-    [
-        ["1", "2", "3", "A"],
-        ["4", "5", "6", "B"],
-        ["7", "8", "9", "C"],
-        ["*", "0", "#", "D"],
-    ], // keypad layout
-    [25, 8, 7, 1], // row GPIO pins
-    [12, 16, 20, 21] // colum GPIO pins
-)
-
 
 function displayText(msg, line, tick) {
     cfg = config[line];
