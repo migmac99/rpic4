f3df9beea9c4c99f3ef969722afa99c668388cac
migmac99 <migmacthegamer@gmail.com>
2022-05-07 14:51:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -68,25 +68,25 @@ function start() {
 
 ////////////////////////////////////////////////////////////////////////
 
-var Keypad = require("rpi-keypad")
+// var Keypad = require("rpi-keypad")
 
-var input = new Keypad(
-    [
-        ["1", "2", "3", "A"],
-        ["4", "5", "6", "B"],
-        ["7", "8", "9", "C"],
-        ["*", "0", "#", "D"],
-    ], // keypad layout
-    [40, 38, 36, 32], // row GPIO pins
-    [37, 35, 33, 31], // colum GPIO pins
-    // additional:
-    true, // use key press events
-    100 // interval in ms to poll for key events
-)
+// var input = new Keypad(
+//     [
+//         ["1", "2", "3", "A"],
+//         ["4", "5", "6", "B"],
+//         ["7", "8", "9", "C"],
+//         ["*", "0", "#", "D"],
+//     ], // keypad layout
+//     [40, 38, 36, 32], // row GPIO pins
+//     [37, 35, 33, 31], // colum GPIO pins
+//     // additional:
+//     true, // use key press events
+//     100 // interval in ms to poll for key events
+// )
 
-input.on("keypress", (key) => {
-    console.log("key pressed: " + key);
-})
+// input.on("keypress", (key) => {
+//     console.log("key pressed: " + key);
+// })
 
 ////////////////////////////////////////////////////////////////////////
 
