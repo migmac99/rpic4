2e39ff942237728a660e3e661a76957acf2e665c
migmac99 <migmacthegamer@gmail.com>
2022-05-07 11:20:14 +0100

    9

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function start() {
     config[0].msg = twinkle
     config[0].scrollSpeed = 100
 
-    config[1].msg = '    8Pirats    '
+    config[1].msg = '    9Pirats    '
     config[1].scroll = true
     config[1].scrollRight = false
     config[1].scrollSpeed = 500
